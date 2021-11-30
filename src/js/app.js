238050aa47820097a17f05442a600072b2915220
Gods1son <tolulopeibiyode@gmail.com>
2021-11-29 20:02:30 -0500

    cleaned up version

--- src/js/app.js
+++ src/js/app.js
@@ -16,15 +16,6 @@ import '../css/app.css';
 import routes from './routes.js';
 
 import List from 'list.js';
-import PouchDB from 'pouchdb';
-//import cordovaSqlitePlugin from 'pouchdb-adapter-cordova-sqlite';
-import pouchFind from 'pouchdb-find';
-//import pouchAuth from 'pouchdb-authentication';
-
-//PouchDB.plugin(cordovaSqlitePlugin);
-PouchDB.plugin(pouchFind);
-//PouchDB.plugin(pouchAuth);
-window.PouchDB = PouchDB;
 
 window.List = List;
 
